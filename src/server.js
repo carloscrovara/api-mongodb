14262dd5a663297f15d1af0b8202a6b05477d4c2
carloscrovara <carloscrovara@gmail.com>
2021-01-08 13:33:03 -0300

    Version 1.1

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ const express = require('express');
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 app.set('port', process.env.PORT || 4000);
-
 
 //LLAMADO A RUTAS
 app.use(require('./routes/cursosRoutes'));
